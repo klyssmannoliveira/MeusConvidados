d418e9ed2656246e4f1f9d8ddec996dec179f45a
klyssmannoliveira <56873266+klyssmannoliveira@users.noreply.github.com>
2021-03-17 15:00:41 -0300

    Versão 4: inclusão dos códigos do hiterson sem a revisao da prof.

--- MeusConvidados/src/Trabalho/Evento.java
+++ MeusConvidados/src/Trabalho/Evento.java
@@ -5,8 +5,9 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class Evento {
-	SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy"); // instancia o formato simples da data
-	private String nomeEvento; // nome do evento dando liberdade ao usuario colocar qualquer nome
+
+	SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy"); // instância o formato simples da data
+	private String nomeEvento; // nome do evento dando liberdade ao usuario para colocar qualquer nome
 	private Date dataEvento; // data do evento no formato Date
 	private int horaInicio; // hora de inicio Ex: 20
 	private int horaTermino; // hora do termino Ex: 23
@@ -40,8 +41,37 @@ public class Evento {
 		bdConvidados.add(convidado);
 	}
 
+	// Atualizar Convidado
+	public void atualizarConvidados() {
+
+	}
+
+	// Remover convidado
+
+	public void removerConvidado(String convidado) {
+		int cont = 0;
+		for (int i = 0; i < bdConvidados.size(); i++) {
+			Convidado aux = bdConvidados.get(i);
+			if (aux.getNome() == convidado) {
+				bdConvidados.remove(i);
+				System.out.println("Convidado removido com sucesso!!\n");
+				cont--;
+				break;
+			}
+			cont++;
+		}
+
+		if (cont == bdConvidados.size()) {
+
+			System.out.println("Convidado não encontrado!!\n");
+
+		}
+
+	}
+
 	// Calculo do total de convidados
 	public void setTotalConvidados() {
+		totalConvidados = 0;
 		for (int i = 0; i < bdConvidados.size(); i++) {
 			Convidado aux = bdConvidados.get(i);
 			totalConvidados = totalConvidados + 1 + aux.getQuantAcompanhante();
@@ -51,7 +81,7 @@ public class Evento {
 	//// Listar tarefa
 	public void listarTarefas() {
 		int quantidade = 0;
-		
+
 		System.out.print("*** Lista de tarefas para o Evento: " + nomeEvento + " ***\n");
 		for (int i = 0; i < bdTarefas.size(); i++) {
 			Tarefa aux = bdTarefas.get(i);
@@ -66,6 +96,29 @@ public class Evento {
 	// Adicionar Tarefa
 	public void adicionarTarefa(Tarefa tarefa) {
 		bdTarefas.add(tarefa);
+	}
+
+	// Remover Tarefa
+
+	public void removerTarefa(String descricao) {
+		int cont = 0;
+		for (int i = 0; i < bdTarefas.size(); i++) {
+			Tarefa aux = bdTarefas.get(i);
+
+			if (aux.getDescricao() == descricao) {
+				bdTarefas.remove(i);
+				System.out.println("Tarefa removida com sucesso!!\n");
+				cont--;
+				break;
+			}
+			cont++;
+		}
+
+		if (cont == bdTarefas.size()) {
+
+			System.out.println("Tarefa não encontrada!!\n");
+		}
+
 	}
 
 	// Calculo da taxa de tarefas
@@ -83,7 +136,7 @@ public class Evento {
 	}
 
 	// Listar Gastos
-	public void listarGasto() {
+	public void listarGastos() {
 		System.out.printf("*** Lista de gastos para o Evento: " + nomeEvento + " ***");
 		for (int i = 0; i < bdGastos.size(); i++) {
 			Gasto aux = bdGastos.get(i);
@@ -98,14 +151,48 @@ public class Evento {
 		bdGastos.add(convidado);
 	}
 
+	// Remover Gastos
+	public void removerGasto(String descricao) {
+		int cont = 0;
+		for (int i = 0; i < bdGastos.size(); i++) {
+			Gasto aux = bdGastos.get(i);
+			if (aux.getDescricao() == descricao) {
+				bdGastos.remove(i);
+				System.out.println("Gasto removido com sucesso!!\n");
+				cont--;
+				break;
+			}
+			cont++;
+		}
+
+		if (cont == bdGastos.size()) {
+
+			System.out.println("Gasto não encontrado!!\n");
+		}
+
+	}
+
 	// Calculo do total de gastos
 	public void setTotalGasto() {
+		totalGasto = 0;
 		for (int i = 0; i < bdGastos.size(); i++) {
 			Gasto aux = bdGastos.get(i);
 			totalGasto = totalGasto + aux.getValorTotal();
 		}
 	}
 
+
+	// Relatório 
+	
+	public void relatorio() {
+		
+		System.out.println("Relatório do evento");
+		listarConvidados();
+		listarTarefas();
+		listarGastos();
+
+	}
+	
 	// Getters e setters
 	public String getNomeEvento() {
 		return nomeEvento;
